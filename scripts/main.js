b856ec5007992ec0c29fd7aedb3836211bf9e0b8
FrancoisleRoux <159991396+231256leRouxFNF@users.noreply.github.com>
2024-10-03 12:50:02 +0200

    ....

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-//Collect data from API
+// //Collect data from API
 console.log("howdy");
 let moviesArray;
 let buttonClicked=0; 
@@ -29,28 +29,6 @@ const requestOptions = {
          })
    .catch((error) => console.error(error));
 }();
-
-function MineMovies(temp) {
-    console.log("Here is temp"+temp);
-    //Creating a new array with what we need
-    moviesArray = temp.map(item => {
-        return {
-            title: item.title,
-            rating: item.imdbRating,
-            image: item.image,
-            length: item.timeline
-        }
-    });
-    console.log(moviesArray);
-}
-function DisplayData(){
-    let temp=moviesArray[buttonClicked].title+""+moviesArray[buttonClicked].rating+""+moviesArray[buttonClicked].length+"";
-    let tempImg= moviesArray[buttonClicked].image;
-    console.log("Here is temp"+temp);
-    //img.src=tempImg; //For displaying images
-    //imageHTML.appendChild(img);    //Need to fix this for image display
-    buttonClicked++;
-}
 
 // Log in / Sign up
 
@@ -145,37 +123,6 @@ else{
 // Homepage / index
 
 //Collect data from API
-console.log("howdy");
-let moviesArray;
-let buttonClicked=0; 
-//let imageHTML=document.getElementById('body'); //Image link to page, must be changed
-//let img=document.createElement('img');
-
-const myHeaders = new Headers();
-myHeaders.append("x-apihub-key", "lYrj-KOMtKiStLzuttl-a4IvbOh3bL-xsnYqkn7iSH0pSHYPSY");
-myHeaders.append("x-apihub-host", "Movies-Verse.allthingsdev.co");
-myHeaders.append("x-apihub-endpoint", "611cdfda-546d-4cc9-91ab-bfdac3194613");
-
-const requestOptions = {
-   method: "GET",
-   headers: myHeaders,
-   redirect: "follow"
-};
-
-!async function() {
-    await fetch("https://Movies-Verse.proxy-production.allthingsdev.co/api/movies/most-popular-movies", requestOptions)
-   .then((response) => response.text())
-   .then((result) => {console.log(result);
-             let JSONResult=JSON.parse(result);
-             //Everything is stored in an object called movies. As such we need JSONResult.movies
-             console.log("The result of JSonparse:"+JSONResult.movies);
-             //Parameter
-             MineMovies(JSONResult.movies);
-             DisplayData();
-         })
-   .catch((error) => console.error(error));
-}();
-
 function MineMovies(temp) {
     console.log("Here is temp"+temp);
     //Creating a new array with what we need
